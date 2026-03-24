79ef49662b4e03bf2c4109d0824c3413c82be831
spanlio <spanlio>
2026-03-24 21:42:15 +0200

    vairs neradas 404 errors

---
+++ admin/aktualitates.php
@@ -0,0 +1,21 @@
+<?php
+    require "header.php";
+?>
+
+<h1>Aktualitātes (Admin)</h1>
+
+<button id="new-btn">+ Pievienot</button>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Virsraksts</th>
+            <th>Statuss</th>
+            <th>Darbības</th>
+        </tr>
+    </thead>
+    <tbody id="table-body"></tbody>
+</table>
+
+<script src="aktualitates-admin.js"></script>
